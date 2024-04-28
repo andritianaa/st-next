eb49cb4e253ec5dc63e5cb3a2d370e5b079a597d
andritianaa <andritianasteve@gmail.com>
2024-04-28 11:01:43 +0300

    initial commit

---
+++ src/components/ui/collapsible.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import {
+  Root,
+  CollapsibleTrigger as Trigger,
+  CollapsibleContent as Content,
+} from "@radix-ui/react-collapsible";
+
+const Collapsible = Root;
+
+const CollapsibleTrigger = Trigger;
+
+const CollapsibleContent = Content;
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent };
